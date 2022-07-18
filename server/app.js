ef3a5505c41db07eca119a44bd5b97b955caec64
Marcelo Makoto <ma_makoto_goto@hotmail.com>
2022-07-17 21:38:36 -0300

    x

--- server/app.js
+++ server/app.js
@@ -1,43 +1,47 @@
 // importação de dependência(s)
+import express from "express";
+import fs from "fs";
+import hbs from "express-handlebars";
+import _ from "underscore";
 
+const jogadores = JSON.parse(
+    fs.readFileSync("./server/data/jogadores.json", "utf8")
+);
 
-// variáveis globais deste módulo
-const PORT = 3000
-const db = {}
+const jogos = JSON.parse(
+    fs.readFileSync("./server/data/jogosPorJogador.json", "utf8")
+);
 
+const PORT = 3000;
+const db = { jogadores, jogos };
+const app = express();
+app.engine("hbs", hbs.engine({ extname: "hbs", defaultLayout: "main" }));
+app.set("view engine", "hbs");
+app.set("views", "server/views");
 
-// carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
-// você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
-// dica: 1-4 linhas de código (você deve usar o módulo de filesystem (fs))
+app.use(express.static("client"));
+app.listen(PORT, () => {
+    console.log("Escutando em: http://localhost:3000");
+});
 
+app.get("/", (request, response) => {
+    response.render("index", { db }); // desenha a view 'index'
+});
 
+app.get("/jogador/:id/", (request, response) => {
+    const res = db.jogadores.players.find((player) => {
+        if (player.steamid == request.params.id) {
+            return player;
+        }
+    });
 
+    const res2 = db.jogos[request.params.id];
 
-// configurar qual templating engine usar. Sugestão: hbs (handlebars)
-//app.set('view engine', '???qual-templating-engine???');
-//app.set('views', '???caminho-ate-pasta???');
-// dica: 2 linhas
-
-
-// EXERCÍCIO 2
-// definir rota para página inicial --> renderizar a view index, usando os
-// dados do banco de dados "data/jogadores.json" com a lista de jogadores
-// dica: o handler desta função é bem simples - basta passar para o template
-//       os dados do arquivo data/jogadores.json (~3 linhas)
-
-
+    response.render("jogador", { res, res2 });
+});
 
 // EXERCÍCIO 3
 // definir rota para página de detalhes de um jogador --> renderizar a view
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
 // dica: o handler desta função pode chegar a ter ~15 linhas de código
-
-
-// EXERCÍCIO 1
-// configurar para servir os arquivos estáticos da pasta "client"
-// dica: 1 linha de código
-
-
-// abrir servidor na porta 3000 (constante PORT)
-// dica: 1-3 linhas de código
